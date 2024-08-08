e11d5121aa07636720b911f33fe50c58067b7404
BeardedSavage <138344263+BeardedSavage@users.noreply.github.com>
2024-08-07 23:17:19 -0500

    I tried. Due date moved from Monday and threw me off

--- final_project/router/booksdb.js
+++ final_project/router/booksdb.js
@@ -1,14 +1,14 @@
 let books = {
-      1: {"author": "Chinua Achebe","title": "Things Fall Apart", "reviews": {} },
-      2: {"author": "Hans Christian Andersen","title": "Fairy tales", "reviews": {} },
-      3: {"author": "Dante Alighieri","title": "The Divine Comedy", "reviews": {} },
-      4: {"author": "Unknown","title": "The Epic Of Gilgamesh", "reviews": {} },
-      5: {"author": "Unknown","title": "The Book Of Job", "reviews": {} },
-      6: {"author": "Unknown","title": "One Thousand and One Nights", "reviews": {} },
-      7: {"author": "Unknown","title": "Nj\u00e1l's Saga", "reviews": {} },
-      8: {"author": "Jane Austen","title": "Pride and Prejudice", "reviews": {} },
-      9: {"author": "Honor\u00e9 de Balzac","title": "Le P\u00e8re Goriot", "reviews": {} },
-      10: {"author": "Samuel Beckett","title": "Molloy, Malone Dies, The Unnamable, the trilogy", "reviews": {} }
+      1: {"author": "Chinua Achebe","title": "Things Fall Apart", "reviews": {}, "isbn": "1254989632494" },
+      2: {"author": "Hans Christian Andersen","title": "Fairy tales", "reviews": {}, "isbn": "4897624821258" },
+      3: {"author": "Dante Alighieri","title": "The Divine Comedy", "reviews": {}, "isbn": "3549785315469" },
+      4: {"author": "Unknown","title": "The Epic Of Gilgamesh", "reviews": {}, "isbn": "8972648203328" },
+      5: {"author": "Unknown","title": "The Book Of Job", "reviews": {}, "isbn": "7832589746582" },
+      6: {"author": "Unknown","title": "One Thousand and One Nights", "reviews": {}, "isbn": "8468777972156" },
+      7: {"author": "Unknown","title": "Nj\u00e1l's Saga", "reviews": {}, "isbn": "4687982354687" },
+      8: {"author": "Jane Austen","title": "Pride and Prejudice", "reviews": {}, "isbn": "2649784329843" },
+      9: {"author": "Honor\u00e9 de Balzac","title": "Le P\u00e8re Goriot", "reviews": {}, "isbn": "7819746874662" },
+      10: {"author": "Samuel Beckett","title": "Molloy, Malone Dies, The Unnamable, the trilogy", "reviews": {}, "isbn": "1649598231379" }
 }
 
 module.exports=books;
